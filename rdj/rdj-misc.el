28e4701af60718c04f4ec99fc3ac09a294048fe1
Ryan D Johnson <ryandjohnson@gmail.com>
2011-09-28 20:09:57 -0700

    Move paren mode to theme

--- rdj/rdj-misc.el
+++ rdj/rdj-misc.el
@@ -1,9 +1,6 @@
 ;; Don't require full "yes" or "no" answers.
 (fset 'yes-or-no-p 'y-or-n-p)
 
-;; Highlight paren expressions
-(show-paren-mode 1)
-(setq show-paren-style 'expression)
 
 ;; Use pathname to get unique buffer names instead of <2>
 (require 'uniquify)
--- rdj/rdj-theme.el
+++ rdj/rdj-theme.el
@@ -1,3 +1,7 @@
+;; Highlight paren expressions
+(show-paren-mode 1)
+(setq show-paren-style 'paren)
+
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'zenburn)
 
