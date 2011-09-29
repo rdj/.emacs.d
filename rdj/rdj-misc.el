64046ccff9f44d1d36125a69323e75335aa11252
Ryan D Johnson <ryandjohnson@gmail.com>
2011-09-28 20:30:58 -0700

    Bring over a couple more misc settings

--- rdj/rdj-misc.el
+++ rdj/rdj-misc.el
@@ -1,9 +1,33 @@
+;;; Behavior
+
 ;; Don't require full "yes" or "no" answers.
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Nobody double-spaces after periods in the 21st century.
+(setq sentence-end-double-space nil)
+
+;; If I turn on truncate-lines, I mean it
+(setq truncate-partial-width-windows nil)
 
 ;; Use pathname to get unique buffer names instead of <2>
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
+
+
+;;; Whitespace/encoding related
+
+;; UTF-8 FTW
+(prefer-coding-system 'utf-8)
+
+;; Always add a final newline
+(setq require-final-newline 't)
+
+;; Never use tabs
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
+;; Scrolling past the end of the file should not make new lines
+(setq next-line-add-newlines nil)
+
 
 (provide 'rdj-misc)
