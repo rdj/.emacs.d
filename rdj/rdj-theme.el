79bcd410ba0aaf2125effa1079ea0b2e4ea96b4d
Ryan D Johnson <ryandjohnson@gmail.com>
2011-09-28 20:24:02 -0700

    Migrate over some display settings, including new size-indication-mode

--- rdj/rdj-theme.el
+++ rdj/rdj-theme.el
@@ -2,6 +2,22 @@
 (show-paren-mode 1)
 (setq show-paren-style 'paren)
 
+;; Show file position
+(line-number-mode 1)
+(column-number-mode 1)
+(size-indication-mode 1)
+
+;; Window title
+(defun rdj-shorten-filename (filename)
+  (replace-regexp-in-string (format "^%s" (getenv "HOME")) "~" filename))
+(setq frame-title-format (list "%b"
+                               (list 'buffer-file-name
+                                     (list " : " '(:eval (rdj-shorten-filename buffer-file-name))))
+                               (list 'dired-directory
+                                     (list " : " '(:eval (rdj-shorten-filename dired-directory))))))
+
+
+;; Color theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'zenburn)
 
