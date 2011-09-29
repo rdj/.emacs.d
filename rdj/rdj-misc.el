34141e17e4f2a959a7a31aed9026340f677193b4
Ryan D Johnson <ryandjohnson@gmail.com>
2011-09-28 19:58:15 -0700

    Highlight paren expressions

--- rdj/rdj-misc.el
+++ rdj/rdj-misc.el
@@ -1,6 +1,10 @@
 ;; Don't require full "yes" or "no" answers.
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Highlight paren expressions
+(show-paren-mode 1)
+(setq show-paren-style 'expression)
+
 ;; Use pathname to get unique buffer names instead of <2>
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
