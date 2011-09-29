eb8bc66e4cf3f8480afb53ecbe64ce735d03c404
Ryan D Johnson <ryandjohnson@gmail.com>
2011-09-28 19:47:26 -0700

    keys for isearch

--- rdj/rdj-bindings.el
+++ rdj/rdj-bindings.el
@@ -2,7 +2,16 @@
 (setq mac-option-modifier nil)
 
 (global-set-key (kbd "C-c C-g") 'magit-status)
+(global-set-key (kbd "C-c C-i") 'toggle-case-fold-search)
 (global-set-key (kbd "C-c C-w") 'rdj-toggle-whitespace-cleanup)
 (global-set-key (kbd "C-c C-z") 'rdj-smartsize-frame)
+
+(add-hook 'isearch-mode-hook
+	  (function
+	   (lambda ()
+	     (define-key isearch-mode-map "\C-h" 'isearch-mode-help)
+	     (define-key isearch-mode-map "\C-t" 'isearch-toggle-regexp)
+	     (define-key isearch-mode-map "\C-c" 'isearch-toggle-case-fold)
+	     (define-key isearch-mode-map "\C-j" 'isearch-edit-string))))
 
 (provide 'rdj-bindings)
