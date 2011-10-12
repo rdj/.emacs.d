28af319a635a2b2d351c088bdb7885448fab9253
Ryan D Johnson <ryandjohnson@gmail.com>
2011-10-11 18:13:03 -0700

    Allow encoding: binary from ruby files

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes (quote ("4b4d806a79d2304ed7896072120d0f4376401843" default))))
+ '(safe-local-variable-values (quote ((encoding . binary) (rdj-whitespace-should-cleanup-p . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
