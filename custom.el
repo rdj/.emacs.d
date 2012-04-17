737409af7494d39af0673fd53b5997a7747c5939
Ryan D Johnson <ryandjohnson@gmail.com>
2012-04-17 14:34:19 -0700

    Update allowed themes to include solarized light and dark

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("9e4712282e027cb1c74087e6ff3889ab70ddc0f6" default)))
+ '(custom-safe-themes (quote ("1440d751f5ef51f9245f8910113daee99848e2c0" "485737acc3bedc0318a567f1c0f5e7ed2dfde3fb" "9e4712282e027cb1c74087e6ff3889ab70ddc0f6" default)))
  '(safe-local-variable-values (quote ((encoding . binary) (rdj-whitespace-should-cleanup-p . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
