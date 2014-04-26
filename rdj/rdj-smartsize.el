4af4127037739a7b76fe9c9efa8a32b448ee4044
Ryan D Johnson <ryandjohnson@gmail.com>
2014-04-26 10:26:01 -0700

    smartsize for rMBP screen

--- rdj/rdj-smartsize.el
+++ rdj/rdj-smartsize.el
@@ -31,10 +31,11 @@
 
 (defun rdj-smartsize-frame-for (display-width display-height)
   (apply 'rdj-setup-frame (cond    ; +X  +Y   WxH  FONT
-    ((= display-width 1680)   (list   1   0 184 52 (rdj-huge-font)))
+    ((= display-width 1440)   (list   0  22 177 46 (rdj-big-font)))
+    ((= display-width 1680)   (list   1  22 184 52 (rdj-huge-font)))
     ((= display-width 1920)   (list 175  50 225 72 (rdj-small-font)))
     ((= display-width 2560)   (list 240  50 260 75 (rdj-big-font)))
-    ((< display-width 1920)   (list   0   0 180 50 (rdj-small-font)))
+    ((< display-width 1920)   (list   0  22 180 50 (rdj-small-font)))
     ((< display-height 1600)  (list 261  50 260 76 (rdj-big-font)))
     ((>= display-height 1920) (list 261 125 260 80 (rdj-big-font)))
   ))
