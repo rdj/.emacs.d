cb5bea05a8e3020d0c64f016001b1df15991bb6a
Ryan D Johnson <ryandjohnson@gmail.com>
2015-04-08 11:43:52 -0700

    Disable marmalade package repo

--- rdj/rdj-package.el
+++ rdj/rdj-package.el
@@ -27,8 +27,8 @@
 
 ;; Bootstrap package.el
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;(add-to-list 'package-archives
+;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
