3d2c98cbba216be65f0cbdf12c9b98d28694def1
Ryan D Johnson <ryandjohnson@gmail.com>
2021-05-05 21:16:10 -0700

    Fix melpa URL

--- rdj/rdj-package.el
+++ rdj/rdj-package.el
@@ -29,8 +29,7 @@
 (require 'package)
 ;(add-to-list 'package-archives
 ;             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (setq rdj-packages-refreshed nil)
