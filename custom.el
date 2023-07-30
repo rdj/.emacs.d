33142b7b4d23e592e72519011337c69282424f8f
Ryan D Johnson <ryandjohnson@gmail.com>
2023-07-30 11:47:47 -0700

    Updates to custom.el

--- custom.el
+++ custom.el
@@ -7,10 +7,13 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
-   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "c4465c56ee0cac519dd6ab6249c7fd5bb2c7f7f78ba2875d28a50d3c20a59473" "01ce486c3a7c8b37cf13f8c95ca4bb3c11413228b35676025fdf239e77019ea1" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "baed08a10ff9393ce578c3ea3e8fd4f8c86e595463a882c55f3bd617df7e5a45" "54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "0049f3436c8d2fa972b7900d8e4777d295b9a99b13d2c26d0627bab64cae128c" default))
+   '("05626f77b0c8c197c7e4a31d9783c4ec6e351d9624aa28bc15e7f6d6a6ebd926" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "4e753673a37c71b07e3026be75dc6af3efbac5ce335f3707b7d6a110ecb636a3" "c4465c56ee0cac519dd6ab6249c7fd5bb2c7f7f78ba2875d28a50d3c20a59473" "01ce486c3a7c8b37cf13f8c95ca4bb3c11413228b35676025fdf239e77019ea1" "e80932ca56b0f109f8545576531d3fc79487ca35a9a9693b62bf30d6d08c9aaf" "baed08a10ff9393ce578c3ea3e8fd4f8c86e595463a882c55f3bd617df7e5a45" "54d1bcf3fcf758af4812f98eb53b5d767f897442753e1aa468cfeb221f8734f9" "0049f3436c8d2fa972b7900d8e4777d295b9a99b13d2c26d0627bab64cae128c" default))
  '(default-input-method "japanese")
+ '(package-selected-packages
+   '(py-autopep8 python-mode ripgrep dracula-theme go-mode lsp-mode rust-mode yaml-mode unicode-fonts swift-mode smex ruby-electric plantuml-mode php-mode magit helm-projectile helm-ag haml-mode flx-ido exec-path-from-shell csharp-mode color-theme-sanityinc-tomorrow coffee-mode auctex ahk-mode ag))
  '(safe-local-variable-values
-   '((encoding . binary)
+   '((rustic-test-arguments . "-- --show-output")
+     (encoding . binary)
      (rdj-whitespace-should-cleanup-p . t))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
